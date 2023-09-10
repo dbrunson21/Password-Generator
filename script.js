489eb33bc5cd747420d4a04f275da6ad05600531
dbrunson21 <brunsondavonte@gmail.com>
2023-09-09 20:29:43 -0400

    cloned html, js, and css files. added password chars

---
+++ script.js
@@ -0,0 +1,19 @@
+var upppercaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var lowercaseChars = "abcdefghijklmnopqrstuvwxyz";
+var numberChars = "0123456789";
+var specialChars = "!@#$%^&*()<>;:?[]{}_-"
+
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
