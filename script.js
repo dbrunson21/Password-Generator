a989a85a47d6a6acf76093d28e27a9af96ce4c77
dbrunson21 <brunsondavonte@gmail.com>
2023-09-11 19:46:18 -0400

    modifed js and fixed css button

--- script.js
+++ script.js
@@ -1,37 +1,48 @@
-// Assignment Code
+
 var generateBtn = document.querySelector("#generate");
+//add event listener to genrerate button
+generateBtn.addEventListener("click", writePassword);
 
-// Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  //retrive user inputs
+  var lengthInput = document.querySelector("#length");
+  var uppercaseInput = document.querySelector("#uppercase");
+  var lowercaseInput = document.querySelector("#lowercase");
+  var numbersInput = document.querySelector("#numbers");
+  var specialInput = document.querySelector("#special");
+  var passwordOutput = document.querySelector("#password");
 
-  passwordText.value = password;
-
-}
-
-// Add event listener to generate button
-
-document.addEventListener("DOMContentLoaded", function () {
-
-  generateBtn.addEventListener("click", writePassword);
-
-  function generatePassword() {
-    var length = parseInt(lengthInput.value);
-    var includeUppercase = uppercaseInput.checked;
-    var includeLowercase = lowercaseInput.checked;
-    var inlcudeNumbers = numbersInput.checked;
-    var includeSpecial = specialInput.checked;
-
-    var upppercaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    var lowercaseChars = "abcdefghijklmnopqrstuvwxyz";
-    var numberChars = "0123456789";
-    var specialChars = "!@#$%^&*()<>;:?[]{}_-";
-
-    let allChars = "";
-    if (includeUppercase) allChars += upppercaseChars;
-    if (includeLowercase) allChars += lowercaseChars;
-    if (inlcudeNumbers) allChars += numberChars;
-    if (includeSpecial) allChars += specialChars;
+  //validate the length input
+  var length = parseInt(lengthInput.value);
+  if (isNaN(length) || length < 6 || length > 128) {
+    alert("Please enter a valid password length between 6 and 128 characters. ");
+    return;
   }
-});
+
+  //character sets
+  var uppercaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var lowercaseChars = "abcdefghijklmnopqrstuvwxyz";
+  var numberChars = "0123456789";
+  var specialChars = "!@#$%^&*()<>;:?[]{}_-";
+
+  var allChars = "";
+  if (uppercaseInput.checked) allChars += uppercaseChars;
+  if (lowercaseInput.checked) allChars += lowercaseChars;
+  if (numbersInput.checked) allChars += numberChars;
+  if (specialInput.checked) allChars += specialChars;
+
+  //if character requirements aren't met
+  if (allChars === "") {
+    alert("Please select at least one character set. ")
+    return;
+  }
+
+  var password = "";
+  for (let i = 0; i < length; i++) {
+    var randomIndex = Math.floor(Math.random() * allChars.length);
+    password += allChars.charAt(randomIndex)
+  }
+
+  //display the gernerated password
+  passwordOutput.value = password;
+}  
